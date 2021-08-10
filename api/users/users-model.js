19ba1b2aacb6382516466ad973e7b326965576b8
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-09 22:06:43 -0700

    :construction: Users-Model
    find, findBy, findById fns completed.

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name');
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -21,6 +24,10 @@ function find() {
 }
 
 function findBy(filter) {
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where(filter);
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -37,6 +44,10 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('user_id', user_id);
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
@@ -68,20 +79,20 @@ function findById(user_id) {
   }
  */
 async function add({ username, password, role_name }) { // done for you
-  let created_user_id
+  let created_user_id;
   await db.transaction(async trx => {
-    let role_id_to_use
+    let role_id_to_use;
     const [role] = await trx('roles').where('role_name', role_name)
     if (role) {
-      role_id_to_use = role.role_id
+      role_id_to_use = role.role_id;
     } else {
       const [role_id] = await trx('roles').insert({ role_name: role_name })
-      role_id_to_use = role_id
+      role_id_to_use = role_id;
     }
     const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
-    created_user_id = user_id
+    created_user_id = user_id;
   })
-  return findById(created_user_id)
+  return findById(created_user_id);
 }
 
 module.exports = {
